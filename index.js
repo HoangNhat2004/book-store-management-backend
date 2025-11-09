025567e557b190e9f9b025a50528d38815721732
HoangNhat2004 <nhnhat202@gmail.com>
2025-11-09 16:57:18 +0700

    Update index

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'https://book-store-management-frontend.vercel.app',
-        'https://book-store-management-frontend-72u77ktpv.vercel.app/',
         'https://*.vercel.app'  // Hỗ trợ preview URLs
     ],
     credentials: true
